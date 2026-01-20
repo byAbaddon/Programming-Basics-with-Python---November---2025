2aa95c8e7167853a2105feabd9d66fd7f6ad19c2
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2026-01-20 20:03:57 +0200

    Add files via upload

---
+++ 1.0 First Steps in Coding - Lab/07. Projects Creation.py
@@ -0,0 +1,7 @@
+name, pr = input(), int(input())
+print(f'The architect {name} will need {pr * 3} hours to complete {pr} project/s.')
+
+'''
+George
+4
+'''
---
+++ 1.0 First Steps in Coding - Lab/08. Pet Shop.py
@@ -0,0 +1,2 @@
+food = [float(input()) for x in range(2)]
+print(food[0] * 2.50 + food[1] * 4,end=' lv.')
